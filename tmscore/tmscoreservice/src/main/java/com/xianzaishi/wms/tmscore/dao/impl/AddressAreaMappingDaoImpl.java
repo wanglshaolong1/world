210adc621af62f681caf95e58472148d5405ca41
quyang <quyang@xianzaishi.com>
2017-01-21 19:33:01 +0800

    update a file

--- tmscore/tmscoreservice/src/main/java/com/xianzaishi/wms/tmscore/dao/impl/AddressAreaMappingDaoImpl.java
+++ tmscore/tmscoreservice/src/main/java/com/xianzaishi/wms/tmscore/dao/impl/AddressAreaMappingDaoImpl.java
@@ -3,9 +3,12 @@ package com.xianzaishi.wms.tmscore.dao.impl;
 import com.xianzaishi.wms.tmscore.dao.itf.IAddressAreaMappingDao;
 import com.xianzaishi.wms.tmscore.vo.AddressAreaMappingVO;
 import com.xianzaishi.wms.common.dao.impl.BaseDaoAdapter;
+dfsadfsadf
 
+
+dfsdf
 public class AddressAreaMappingDaoImpl extends BaseDaoAdapter implements IAddressAreaMappingDao {
-public String getVOClassName() {
+publidsfsdfsdfsadfsadfc String getVOClassName() {
 		return "AddressAreaMappingDO";
 	}
 }
